1a1b71310f2f1cd7183ccdaf9a2de7ee42417fe9
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-05-11 23:09:15 -0300

    Delete 1 - Estrutura sequencial directory

--- 1 - Estrutura sequencial/12.py
+++
@@ -1,8 +0,0 @@
-#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
-
-altura = int(input("digite sua altura em cm²: "))
-altura = altura /100
-
-pesoIdeal = (72.7*altura) - 58
-
-print("seu peso ideal é:", pesoIdeal)
