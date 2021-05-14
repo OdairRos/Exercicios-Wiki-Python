b636ceaacc8ea12678d94ec637bd469c9c64ea55
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-05-14 00:26:01 -0300

    Delete 10.py

--- 1 - Estrutura sequencial/10.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-
-tempC = float(input("temperatura 3m Celsius: "))
-
-tempF = tempC * (9/5) + 32
-print(f"{tempC} graus celsius em fahrenheit: {tempF}")
