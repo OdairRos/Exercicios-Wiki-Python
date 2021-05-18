b1ec4956d074a2254507a412cf4f1cd90d35f99b
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-05-17 21:58:41 -0300

    Delete 1 - Estrutura sequencial directory

--- 1 - Estrutura sequencial/7.py
+++
@@ -1,8 +0,0 @@
-#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
-
-lado = float(input("qual medida do lado em cm?: "))
-base = float(input("qual medida da base em cm?: "))
-
-area = base*lado
-
-print("a area do quadrado é: ", area, "cm² e o dobro é:", area*2,"cm²")
